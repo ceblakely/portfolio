ae0a990aceadb0acfbce6dfdbe1ceb1b273f3314
ceblakely <cblakelydev@gmail.com>
2022-01-03 17:59:11 -0600

    FIxing heroku issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const port = 5000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-//app.set("port", process.env.PORT || 5000);
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "static")));
 
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(app.get(port), function () {
+app.listen(port, function () {
   console.log("Congratulations on ur portfolio!!");
 });
